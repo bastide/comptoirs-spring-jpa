505233509af06f5a188e78552df7d8e0193ae40b
Rémi Bastide <Remi.Bastide@irit.fr>
2022-01-17 15:37:12 +0100

    CORS

--- src/main/java/comptoirs/dao/ClientRepository.java
+++ src/main/java/comptoirs/dao/ClientRepository.java
@@ -1,10 +1,6 @@
 package comptoirs.dao;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import comptoirs.entity.Client;
 
 // This will be AUTO IMPLEMENTED by Spring 
